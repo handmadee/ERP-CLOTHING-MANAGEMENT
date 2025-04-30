5162e86e97b012c9fb44449554e76108d56933c5
Dino_datcoder38 <dinodatcoder38@gmail.com>
2025-04-30 17:44:34 +0700

    fly-deploy

--- src/main.ts
+++ src/main.ts
@@ -136,8 +136,8 @@ async function bootstrap() {
   });
 
   // Khởi động server
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
+  // const port = process.env.PORT || 3000;
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 
   // Logging startup information
   Logger.log(`🚀 Environment: ${process.env.NODE_ENV}`);
